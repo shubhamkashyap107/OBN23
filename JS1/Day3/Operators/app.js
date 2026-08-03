ee9c79ea97c57cc6eb4c53ce7ad03d5d6aaa4358
Shubham <shubhamkashyap107@gmail.com>
2026-08-03 14:12:41 +0530

    Commit

---
+++ JS1/Day3/Operators/app.js
@@ -0,0 +1,177 @@
+// console.log(78 + 100)
+// console.log(78 - 100)
+// console.log(78 * 100)
+// console.log(78 / 100)
+// console.log(78 % 100)
+// console.log(3 ** 4)
+
+// var a = 3;
+// var b = 4;
+
+// var c = a + b
+// console.log(c)
+
+// var d = a * b
+// console.log(d)
+
+
+// increment , decrement
+
+
+// var a = 1
+// console.log(a)
+
+
+// a++
+// a++
+// a++
+// a++
+
+// ++a
+// ++a
+// ++a
+// ++a
+
+// a--
+// a--
+// a--
+
+
+// --a
+// --a
+// --a
+// console.log(a)
+
+
+
+
+
+
+// var a = 10
+// console.log(a)
+// console.log(++a)
+
+
+// var c = ++a
+// console.log(c)
+
+// console.log(a)
+// console.log(--a)
+
+// var c = --a
+// console.log(c)
+
+
+// var a = 121
+// console.log(a++)
+// console.log(a--)
+
+// var c = a++
+
+// console.log(c)
+// console.log(a)
+
+
+
+// console.log("Hello" + "World")
+
+
+// var fn = "Gabbar"
+// var ln = "Singh"
+
+// var fullName = fn + " " + ln // concatenation
+// console.log(fullName)
+
+
+// assignment operators
+
+
+// var a = 10
+
+// a = a + 2
+// a += 2 // shorthand
+// a -= 10 // a = a - 10
+
+// a *= 100 // a = a * 100
+// a /= 100 // a = a / 100
+
+// a %= 2 // a = a % 2
+// a **= 2 // a = a ** 2
+
+
+// console.log(a)
+
+
+
+
+
+
+// comparsion operators : boolean (t / f)
+
+
+// console.log(10 == 10)
+// console.log(10 == 11)
+
+// console.log(10 == "10") // typecasting
+// console.log(10 === "10")
+
+// console.log(23 > 90)
+// console.log(23 < 90)
+
+
+// console.log(18 >= 18)
+// console.log(18 <= 18)
+
+
+// console.log(18 != 19)
+
+
+
+
+
+
+// logical opertors
+
+
+// console.log(true && true)
+// console.log(10 > 5 && 4 > 1)
+console.log((3 * 4) && (6 * 2)) // short circuiting
+
+// console.log(true && false)
+// console.log(10 > 5 && 4 < 1)
+
+// console.log(false && true)
+// console.log(10 < 5 && 4 > 1)
+
+// console.log(false && false)
+// console.log(10 < 5 && 4 < 1)
+
+
+// console.log(true || true)
+// console.log(19 > 10 || 5 > 2)
+
+
+// console.log(true || false)
+// console.log(19 > 10 || 5 < 2)
+
+
+// console.log(false || true)
+// console.log(19 < 10 || 5 > 2)
+
+
+// console.log(false || false)
+// console.log(19 < 10 || 5 < 2)
+
+
+
+var age = 19
+
+console.log(!true)
+console.log(!age > 18) 
+
+
+console.log(!false)
+console.log(!age < 18)
+
+
+
