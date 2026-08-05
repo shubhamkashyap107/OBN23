ef9cbb421b4b6dd8448239437eebc735dc95f248
Shubham <shubhamkashyap107@gmail.com>
2026-08-05 14:19:12 +0530

    Commit

---
+++ JS1/Day4/ifElse/app.js
@@ -0,0 +1,47 @@
+// if(true)
+// {
+//     console.log("if block chala")
+// }
+
+// if(false)
+// {
+//     console.log("if block chala")
+// }
+
+// var age = 8
+// if(age >= 18)
+// {
+//     console.log("Enter Website")
+// }
+
+// var n = 352
+// if(n % 2 == 0)
+// {
+//     console.log("Even")
+// }
+
+
+
+var n = 898
+
+if(n % 2 == 0)
+{
+    console.log("Even")
+}
+else
+{
+    console.log("Odd")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ JS1/Day5/Conditionals/app.js
@@ -0,0 +1,105 @@
+// var n = 0
+
+// if(n == 0)
+// {
+//     console.log("Zero")
+// }
+// else if(n < 0)
+// {
+//     console.log("-ve")
+// }
+// else
+// {
+//     console.log("+ve")
+// }
+
+
+// var marks = -6
+// if(marks < 0 || marks > 100)
+// {
+//     console.log("Invalid Marks")
+// }
+// else if(marks > 90)
+// {
+//     console.log("A+")
+// }
+// else if(marks > 80)
+// {
+//     console.log("A")
+// }
+// else if(marks > 70)
+// {
+//     console.log("B")
+// }
+// else if(marks > 60)
+// {
+//     console.log("C")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// if(marks >= 0 && marks <= 60) // if marks are gte 0 and marks lte 60
+// {
+//     console.log("Fail")
+// }
+// else if(marks >= 61 && marks <= 70)
+// {
+//     console.log("C")
+// }
+// else if(marks >= 71 && marks <= 80)
+// {
+//     console.log("B")
+// }
+// else if(marks >= 81 && marks <= 90)
+// {
+//     console.log("A")
+// }
+// else if(marks >= 91 && marks <= 100)
+// {
+//     console.log("A+")
+// }
+// else
+// {
+//     console.log("Invalid marks")
+// }
+
+var m = 22
+
+
+if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+{
+    console.log(31)
+}
+else if(m == 4 || m == 6 || m == 9 || m == 11)
+{
+    console.log(30)
+}
+else if(m == 2)
+{
+    console.log("28/29")
+}
+else
+{
+    console.log("Invalid Month")
+}
+
+
+
+var a = 12789
+var b = 5000
+var c = 500345678
+
+
+if(a > b && a > c)
+{
+    console.log("a")
+}
+else if(b > c)
+{
+    console.log("b")
+}
+else
+{
+    console.log("c")
+}
