f8b649b6a9371d2337f9b00b6a09eb463c0a248a
Shubham <shubhamkashyap107@gmail.com>
2026-08-10 14:25:15 +0530

    Commit

---
+++ JS1/Day8/While/app.js
@@ -0,0 +1,61 @@
+// var i = 1
+
+// while(i <= 5) // infinite time -> 1,2,3,4,5,.....,infinity -> infinite loop
+// {
+//     console.log("NAAM")
+// }
+
+
+// while(i <= 1000) // 1000 time -> 1,2,3,4,5,...,1000 
+// {
+//     console.log("NAAM")
+//     i++
+// }
+
+
+// var n = 4
+// var i = 1
+
+// while(i <= n)
+// {
+//     console.log(i)
+//     i++
+// }
+
+
+// var start = 33
+// var end = 40
+
+// while(start <= end)
+// {
+//     console.log(start)
+//     start++
+// }
+
+
+//counter bana lo
+// var i = start
+
+// while(i <= end)
+// {
+//     console.log(i)
+//     i++
+// }
+
+
+var n = 3
+var i = 1
+
+// while(n > 0)
+// {
+//     console.log(n)
+//     n--
+// }
+
+while(i <= n)
+{
+    console.log(n - i + 1)
+    i++
+}
+
+
---
+++ JS1/Day8/doWhile/app.js
@@ -0,0 +1,26 @@
+// var i = 1
+
+// while(i <= 5)
+// {
+//     console.log("NAME")
+//     i++
+// }
+
+
+// do{
+//     console.log("NAAM")
+//     i++
+// }
+// while(i <= 5)
+
+//ATM & PIN Verification: The machine must prompt you to enter your PIN at least once. It checks if the PIN is correct after you type it, repeating the loop only if it is wrong and you have remaining attempts.
+
+// var input = Number(prompt("Enter your PIN"))
+
+do{
+    var input = prompt("Enter your PIN")
+}
+while(input != "9990")
+
+console.log(input)
+
