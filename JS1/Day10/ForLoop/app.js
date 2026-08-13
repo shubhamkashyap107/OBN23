d55f9ce3c6b5182801ad4b210cdd1f60e7fa3934
Shubham <shubhamkashyap107@gmail.com>
2026-08-13 14:21:11 +0530

    Commit

---
+++ JS1/Day10/ForLoop/app.js
@@ -0,0 +1,81 @@
+// for(let i = 1; i <= 10; i++)
+// {
+//     console.log(i)
+// }
+
+// let j = 1
+
+// for(; j <= 10; j++)
+// {
+//     console.log(j)
+// }
+
+// let k = 1
+// for(;k <= 10;)
+// {
+//     console.log(k)
+//     k++
+// }
+
+// for(;;)
+// {
+    // 
+// }
+
+
+// for(let i = 2; i <= 100; i+=2)
+// {
+    
+//     console.log(i)
+// }
+
+
+// for(let i = 100; i >= 1; i--)
+// {
+//     console.log(i)
+// }
+
+
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     if(i % 3 == 0)
+//     {
+//         // break
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+// let i = 1
+
+// while(i <= 10)
+// {
+//     if(i % 3 == 0)
+//     {
+//         // break
+//         i++
+//         continue
+//     }
+//     console.log(i)
+//     i++
+// }
+
+
+// for(let i = 1; i <= 10; i++)
+// {
+//     if(i % 3 == 0)
+//     {
+//         // break
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+for(let i = 1; i <= 100; i--)
+{
+    console.log(i)
+}
---
+++ JS1/Day11/Functions/app.js
@@ -0,0 +1,59 @@
+function sayHi()
+{
+    console.log("Hello World")
+}
+
+
+// sayHi()
+// console.log("Work1")
+// sayHi()
+// console.log("Work2")
+// sayHi()
+// console.log("Work3")
+
+function sum(x, y)
+{
+    // var sum = x + y
+    // console.log(sum)
+    // console.log(x + y)
+
+
+    // return true
+    // return "ok"
+    return x + y
+}
+// sum(1,2)
+// sum("a", "b")
+
+// let retVal = sum(9,10)
+// console.log(retVal)
+
+
+
+// let ans = "qwertyu".toUpperCase()
+// console.log(ans)
+
+
+
+function xyz()
+{
+    console.log("OK")
+    return 
+}
+
+
+// let retVal = xyz()
+// console.log(retVal)
+
+
+
+// functional expressions
+
+var myFn = function (name)
+{
+    console.log("Hello,", name)
+
+    return true
+}
+
+console.log(myFn("SK"))
