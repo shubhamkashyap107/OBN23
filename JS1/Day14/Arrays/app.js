4604b86c3edd94b2add96a0a0a685ede03eb3b1e
Shubham <shubhamkashyap107@gmail.com>
2026-08-19 14:23:48 +0530

    Commit

---
+++ JS1/Day14/Arrays/app.js
@@ -0,0 +1,123 @@
+// let arr = [1, 2, 3, "abc", false, []]
+
+// console.log(arr.length)
+// console.log(arr)
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
+// console.log(arr[5])
+
+
+// let arr = [1,2,3,4,5,6]
+
+// methods / function
+
+// const newLength = arr.push(17, 18, 19, 20) // des
+// const removedVal = arr.pop()
+
+
+
+// let removedVal =  arr.shift()
+// let newLength = arr.unshift(-1,-2,-3)
+
+
+// let arr = [1,2,3,4,5,6]
+
+// let ans = arr.slice(3) // non des
+// let ans = arr.slice(1, 4) // non des
+// let ans = arr.slice(1, 400) // non des
+
+
+// let ans = arr.slice(-3)
+// let ans = arr.slice(-3, -1)
+// let ans = arr.slice(-3, -5)
+// let ans = arr.slice(1,3)
+
+
+
+// console.log(arr)
+// console.log(ans)
+
+
+
+// let arr = [1,2,3,4,5,6]
+
+// let ans = arr.splice(2, 3)
+// const ans = arr.concat([7,8,9,10])
+// const ans2 = arr.includes(52)
+// const ans3 = arr.indexOf(4)
+// const ans4 = arr.lastIndexOf(4)
+// const ans5 = ["Shubham", "Kashyap"].join(" ")
+// const ans6 = arr.join("-")
+// arr.reverse()
+
+// console.log(arr)
+// // console.log(ans)
+// console.log(ans2)
+// console.log(ans3)
+// console.log(ans4)
+// console.log(ans5)
+// console.log(ans6)
+
+
+let arr = [1,2,3,4,5]
+
+
+
+for(let item of arr)
+{
+    console.log(item)
+}
+
+
+
+
+
+// for(let i = 0; i < arr.length; i++)
+// {
+//     console.log(arr[i])
+// }
+
+
+
+// for(let j = arr.length - 1; j >= 0; j--)
+// {
+//     console.log(arr[j])
+// }
+
+
+
+
+
+
+
+
+
+// let i = 0
+
+// while(i < arr.length)
+// {
+//     console.log(arr[i])
+//     i++
+// }
+
+// let j = arr.length - 1
+
+// while(j >= 0)
+// {
+//     console.log(arr[j])
+//     j--
+// }
+
+
+
+
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+// console.log(arr[4])
