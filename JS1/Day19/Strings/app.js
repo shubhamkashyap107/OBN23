ec953ce98f015e2c972e3a4e7e120be868a4642b
Shubham <shubhamkashyap107@gmail.com>
2026-08-27 14:14:03 +0530

    Commit

---
+++ JS1/Day19/Strings/app.js
@@ -0,0 +1,117 @@
+// let str = 'abcd'
+// let str2 = "abcd"
+// let str3 = `abcd${1234}`
+
+
+// console.log(str)
+// console.log(str2)
+// console.log(str3)
+
+
+
+// let str = "hello world"
+
+// console.log(str.at(1))
+// console.log(str.charAt(0))
+// console.log(str.length)
+// console.log(str.concat("asdfghgfds"))
+// console.log(str + "helo")
+// console.log(str.endsWith("ld"))
+// console.log(str.startsWith("he"))
+// console.log(str.includes("o"))
+// console.log(str.indexOf("l",4))
+// console.log(str.slice(3))
+// console.log(str.slice(3,5))
+
+
+// let msg = "          hello how are you.        "
+
+// console.log(msg)
+// console.log(msg.trim())
+// console.log(msg.trimStart())
+// console.log(msg.trimEnd())
+// console.log(msg.trimLeft())
+// console.log(msg.trimRight())
+
+
+
+
+
+
+// console.log(str.splice())
+
+
+
+
+
+// let str = "AZaz"
+
+// console.log("A" == "a")
+
+// console.log(str.charCodeAt(0)) // A -> 65
+// console.log(str.charCodeAt(1)) // Z -> 90
+// console.log(str.charCodeAt(2)) // a -> 97
+// console.log(str.charCodeAt(3)) // z => 122
+
+
+// for(let i = 65; i <= 90; i++)
+// {
+//     console.log(String.fromCharCode(i))
+// }
+
+
+// for(let i = 97; i <= 122; i++)
+// {
+//     console.log(String.fromCharCode(i))
+// }
+
+
+// console.log("hello \n world")
+
+// for(let i = 0; i <= 200; i++)
+// {
+//     console.log(i, String.fromCharCode(i))
+// }
+
+
+// let str = "abc"
+
+
+// console.log(str.repeat(4))
+// console.log(str.padEnd(6, "*"))
+
+// console.log(Math.ceil(Math.random() * 6))
+
+// let otp = String(Math.floor(Math.random() * 1000000))
+// console.log(otp, otp.padEnd(6, 0))
+
+
+// let str = "hello hey how are you"
+// console.log(str.replace("e", "y"))
+// console.log(str.replaceAll("e", "y"))
+
+
+
+// let str = "hello hey cat apple hellp cat apple"
+// let arr = str.split(" ")
+// console.log(arr)
+
+// console.log(arr.join(" "))
+
+// let arr = [1,2,3]
+// let arr2 = arr
+
+// arr2.push(4)
+
+// console.log(arr)
+// console.log(arr2)
+
+
+// let str = "abc"
+// let str2 = str
+
+// str2 += "!"
+
+
+// console.log(str)
+// console.log(str2)
